c31099996b9ec27039589de14d2a8a0ef978fde0
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-29 21:34:05 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
-INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Rowdy",4,"Male","White","Aggresive",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Tabitha",7,"Female","Brown","Friendly",1);
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Sergeant Brown",4,"Male","Black","Aggresive",1);
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Melissa",5,"Female","Black","Friendly",1);
