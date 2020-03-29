5c4bdaf18bd7d59b5ec2809ef91344eda370b8c7
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-29 20:39:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name) VALUES ("MR. Chocolate","Rowdy","Tabitha","Seargeant Brown","Melissa","Grinch","Wendy",NULL);
+INSERT INTO bears (name) VALUES ("Mr.Chocolate","Rowdy","Tabitha","Sergeant Brown","Melissa","Grinch","Wendy",NULL);
