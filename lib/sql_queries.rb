d2830f52fc5f41db3ce4d53c0e7ab2f1bcd0a695
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-29 21:56:28 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,9 +22,9 @@ def selects_most_prominent_color_and_returns_with_count
   "SELECT color,COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1;"
 end
 
-# def counts_number_of_bears_with_goofy_temperaments
-#   "Write your SQL query here"
-# end
+def counts_number_of_bears_with_goofy_temperaments
+  "Write your SQL query here"
+end
 
 # def selects_bear_that_killed_Tim
 #   "SELECT name FROM bears WHERE name=NULL;"
